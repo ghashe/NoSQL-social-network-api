0331b33ae3ea3e842bfe0105bb3b8085a365df8e
Abyou <emandaabyou@gmail.com>
2022-07-06 17:10:37 -0500

    The model index has been created.

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Importing the User and Thought model
+const User = require("./User");
+const Thought = require("./Thought");
+
+// export the User and Thought model
+module.exports = { User, Thought };
