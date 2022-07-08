b7581ee918ecbfd30a0284de78b5a6c4578c8278
Abyou <emandaabyou@gmail.com>
2022-07-08 01:26:52 -0500

    The user thought routes have been created.

--- controllers/userThought-controller.js
+++ controllers/userThought-controller.js
@@ -2,247 +2,178 @@
 const { User, UserThought } = require("../models");
 
 const userthoughtController = {
-    // The /api/users endpoint
+  // The /api/users endpoint
 
-    // GET all user thoughts (GET /api/thoughts)
-    getAllUserThoughts(request, response) {
-        // Find all UserThought objects in our UserThought model with the .find({}) method
-        UserThought.find({})
-            .populate({
-                path: "userReactions",
-                select: "-__v",
-            })
-            .select("-__v")
-            .sort({ _id: -1 })
-            .then((databaseUserData) => response.json(databaseUserData))
-            // Catching error and sending status code 400 if the server is unable to process the request because of a client error
-            .catch((err) => {
-                console.log(err);
-                response.sendStatus(400);
-            });
-    },
+  // GET all user thoughts (GET /api/thoughts)
+  getAllUserThought(request, response) {
+    // Find all UserThought objects in our UserThought model with the .find({}) method
+    UserThought.find({})
+      .populate({
+        path: "userReactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((databaseUserData) => response.json(databaseUserData))
+      // Catching error and sending status code 400 if the server is unable to process the request because of a client error
+      .catch((err) => {
+        console.log(err);
+        response.sendStatus(400);
+      });
+  },
 
-    // GET UserThought by id (GET /api/thoughts/id)
-    getUserThoughtById({ params }, response) {
-        UserThought.findOne({ _id: params.id })
-            .populate({
-                path: "userReactions",
-                select: "-__v",
-            })
-            .select("-__v")
-            .sort({ _id: -1 })
-            .then((databaseUserData) => {
-                if (!databaseUserData) {
-                    // Sending a status 404 message to the user if thought with the given id is not found
-                    response.status(404).jason({
-                        message: `Sorry, no thought with id ${params.id} has been found! Please check your input and try again!`,
-                    });
-                    return;
-                }
-                // If a thought with the given id is found, convert data to JSON format and send that thought
-                response.json(databaseUserData);
-            })
-            // Catching error and ending status code 400 if the server is unable to process the request because of a client error.
-            .catch((err) => {
-                console.log(err);
-                response.sendStatus(400);
-            });
-    },
+  // GET UserThought by id (GET /api/thoughts/id)
+  getUserThoughtById({ params }, response) {
+    UserThought.findOne({ _id: params.id })
+      .populate({
+        path: "userReactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((databaseUserData) => {
+        if (!databaseUserData) {
+          // Sending a status 404 message to the user if thought with the given id is not found
+          response.status(404).jason({
+            message: `Sorry, no thought with id ${params.id} has been found! Please check your input and try again!`,
+          });
+          return;
+        }
+        // If a thought with the given id is found, convert data to JSON format and send that thought
+        response.json(databaseUserData);
+      })
+      // Catching error and ending status code 400 if the server is unable to process the request because of a client error.
+      .catch((err) => {
+        console.log(err);
+        response.sendStatus(400);
+      });
+  },
 
-    // Create new thought (POST /api/thoughts)
-    createUserThought({ body }, response) {
-        UserThought.create(body)
-            .then(({ _id }) => {
-                return User.findOneAndUpdate(
-                    { _id: body.userId },
-                    { $push: { thoughts: _id } },
-                    { new: true }
-                );
-            })
-            .then((databaseUserData) => {
-                if (!databaseUserData) {
-                    // Sending a status 404 message to the user if user with the given id is not found
-                    response.status(404).jason({
-                        message: `Sorry, no user with id ${_id} has been found! Please check your input and try again!`,
-                    });
-                    return;
-                }
-
-                // If a user with the given id is found, convert data to JSON format and send that user to the client
-                response.json(databaseUserData);
-            })
-            // Sending error if any to the user if the server encountered an unexpected condition that prevented it from fulfilling the request.
-            .catch((err) => {
-                console.log(err);
-                response.json(err);
-            });
-    },
-
-    // Update a thought (PUT /api/thoughts/id)
-    updateUserThought({ params, body }, response) {
-        UserThought.findOneAndUpdate({ _id: params.id }, body, {
-            new: true,
-            runValidators: true,
-        })
-            .then((databaseUserData) => {
-                if (!databaseUserData) {
-                    // Sending a status 404 message to the user if thought with the given id is not found
-                    response.status(404).jason({
-                        message: `Sorry, no thought with id ${params.id} has been found! Please check your input and try again!`,
-                    });
-                    return;
-                }
-
-                // If a thought with the given id is found, convert data to JSON format and send that thought to the client
-                response.json(databaseUserData);
-            })
-            // Sending error if any to the user if the server encountered an unexpected condition that prevented it from fulfilling the request.
-            .catch((err) => {
-                console.log(err);
-                response.json(err);
-            });
-    },
-
-    // Delete thought by id (DELETE /api/thoughts/id)
-    deleteUseerThought({ params }, response) {
-        UserThought.findOneAndDelete({ _id: params.id })
-            .then((databaseUserData) => {
-                if (!databaseUserData) {
-                    // Sending a status 404 message to the user if thought with the given id is not found
-                    response.status(404).jason({
-                        message: `Sorry, no thought with id ${params.id} has been found! Please check your input and try again!`,
-                    });
-                    return;
-                }
-                return User.findOneAndUpdate(
-                    { _id: params.userId },
-                    { $pull: { userThoughts: params.Id } },
-                    { new: true }
-                );
-            })
-            .then((databaseUserData) => {
-                if (!databaseUserData) {
-                    // Sending a status 404 message to the user if user with the given id is not found
-                    response.status(404).jason({
-                        message: `Sorry, no user with id ${params.Id} has been found! Please check your input and try again!`,
-                    });
-                    return;
-                }
-                response.json(databaseUserData);
-            })
-            // Sending error if any to the user if the server encountered an unexpected condition that prevented it from fulfilling the request.
-            .catch((err) => response.json(err));
-    },
-
-
-    // ADD reaction 
-    createUserReaction({ params, body }, response) {
-        UserThought.finfOneAndUpdate(
-            { _is: params.UserThoughtId },
-            { $push: { userReaction: body } },
-            { new: true, runValidators: true })
-            .populate({ path: 'userReactions', select: '-__v' })
-            .select('-__v')
-            .then(databaseUserData => {
-                if (!databaseUserData) {
-                    // Sending a status 404 message to the user if user with the given id is not found
-                    response.status(404).jason({
-                        message: `Sorry, no though with id ${params.UserThoughtId} has been found! Please check your input and try again!`,
-                    });
-                    return;
-                }
-                response.json(databaseUserData);
-            })
-            // Sending error if any to the user if the server encountered an unexpected condition that prevented it from fulfilling the request.
-            .catch((err) => response.json(err));
-
-    },
-    deleteUserReaction({ params }, response) {
-        UserThought.findOneAndUpdate(
-          { _id: params.userThoughtId },
-          { $pull: { userReactions: { userReactionId: params.userReactionId } } },
+  // Create new thought (POST /api/thoughts)
+  createUserThought({ body }, response) {
+    UserThought.create(body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: body.userId },
+          { $push: { thoughts: _id } },
           { new: true }
-        )
-          .then(databaseUserData => {
-            if (!databaseUserData) {
-            // Sending a status 404 message to the user if thought with the given id is not found   
-            response.status(404).jason({
-                message: `Sorry, no though with id ${params.UserThoughtId} has been found! Please check your input and try again!`,
-            });
-            return;
-            }
-           res.json(databaseUserData);
-          })
-          .catch(err => res.json(err));
-      }
-    
-    
-    };
-    
-    module.exports = thoughtController
-  
+        );
+      })
+      .then((databaseUserData) => {
+        if (!databaseUserData) {
+          // Sending a status 404 message to the user if user with the given id is not found
+          response.status(404).jason({
+            message: `Sorry, no user with id ${_id} has been found! Please check your input and try again!`,
+          });
+          return;
+        }
 
+        // If a user with the given id is found, convert data to JSON format and send that user to the client
+        response.json(databaseUserData);
+      })
+      // Sending error if any to the user if the server encountered an unexpected condition that prevented it from fulfilling the request.
+      .catch((err) => {
+        console.log(err);
+        response.json(err);
+      });
+  },
 
+  // Update a thought (PUT /api/thoughts/id)
+  updateUserThought({ params, body }, response) {
+    UserThought.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((databaseUserData) => {
+        if (!databaseUserData) {
+          // Sending a status 404 message to the user if thought with the given id is not found
+          response.status(404).jason({
+            message: `Sorry, no thought with id ${params.id} has been found! Please check your input and try again!`,
+          });
+          return;
+        }
 
+        // If a thought with the given id is found, convert data to JSON format and send that thought to the client
+        response.json(databaseUserData);
+      })
+      // Sending error if any to the user if the server encountered an unexpected condition that prevented it from fulfilling the request.
+      .catch((err) => {
+        console.log(err);
+        response.json(err);
+      });
+  },
 
+  // Delete thought by id (DELETE /api/thoughts/id)
+  deleteUseerThought({ params }, response) {
+    UserThought.findOneAndDelete({ _id: params.id })
+      .then((databaseUserData) => {
+        if (!databaseUserData) {
+          // Sending a status 404 message to the user if thought with the given id is not found
+          response.status(404).jason({
+            message: `Sorry, no thought with id ${params.id} has been found! Please check your input and try again!`,
+          });
+          return;
+        }
+        return User.findOneAndUpdate(
+          { _id: params.userId },
+          { $pull: { userThoughts: params.Id } },
+          { new: true }
+        );
+      })
+      .then((databaseUserData) => {
+        if (!databaseUserData) {
+          // Sending a status 404 message to the user if user with the given id is not found
+          response.status(404).jason({
+            message: `Sorry, no user with id ${params.Id} has been found! Please check your input and try again!`,
+          });
+          return;
+        }
+        response.json(databaseUserData);
+      })
+      // Sending error if any to the user if the server encountered an unexpected condition that prevented it from fulfilling the request.
+      .catch((err) => response.json(err));
+  },
 
-
-
-
-
-
-
-
-
-// ADD friend (/api/users/:userid/fiends/:friendId)
-addFriend({ params }, response) {
-    User.findOneAndUpdate(
-        { _id: params.userId },
-        { $push: { friends: params.friendId } },
-        { new: true }
+  // ADD reaction
+  createUserReaction({ params, body }, response) {
+    UserThought.finfOneAndUpdate(
+      { _is: params.UserThoughtId },
+      { $push: { userReaction: body } },
+      { new: true, runValidators: true }
     )
-        .then((databaseUserData) => {
-            if (!databaseUserData) {
-                // Sending a status 404 message to the user if user with the given id is not found
-                response.status(404).jason({
-                    message: `Sorry, no user with id ${params.userId} has been found! Please check your input and try again!`,
-                });
-                return;
-            }
-            // If a user with the given id is found and added, convert data to JSON format and send that user to the client
-            response.json(databaseUserData);
-        })
-        // Catching error and ending status code 400 if the server is unable to process the request because of a client error.
-        .catch((err) => {
-            console.log(err);
-            response.status(400).json(err);
-        });
-},
-
-// DELETE friend (/api/users/:userid/fiends/:friendId)
-deleteFriend({ params }, response) {
-    User.findOneAndDelete(
-        { _id: params.userId },
-        { $pull: { friends: params.friendId } },
-        { new: true }
+      .populate({ path: "userReactions", select: "-__v" })
+      .select("-__v")
+      .then((databaseUserData) => {
+        if (!databaseUserData) {
+          // Sending a status 404 message to the user if user with the given id is not found
+          response.status(404).jason({
+            message: `Sorry, no though with id ${params.UserThoughtId} has been found! Please check your input and try again!`,
+          });
+          return;
+        }
+        response.json(databaseUserData);
+      })
+      // Sending error if any to the user if the server encountered an unexpected condition that prevented it from fulfilling the request.
+      .catch((err) => response.json(err));
+  },
+  deleteUserReaction({ params }, response) {
+    UserThought.findOneAndUpdate(
+      { _id: params.userThoughtId },
+      { $pull: { userReactions: { userReactionId: params.userReactionId } } },
+      { new: true }
     )
-        .then((databaseUserData) => {
-            if (!databaseUserData) {
-                // Sending a status 404 message to the user if user with the given id is not found
-                response.status(404).jason({
-                    message: `Sorry, no user with id ${params.userId} has been found! Please check your input and try again!`,
-                });
-                return;
-            }
-            // If a user with the given id is found and deleted, convert data to JSON format and send that user to the client
-            response.json(databaseUserData);
-        })
-        // Catching error and ending status code 400 if the server is unable to process the request because of a client error.
-        .catch((err) => {
-            console.log(err);
-            response.status(400).json(err);
-        });
-},
+      .then((databaseUserData) => {
+        if (!databaseUserData) {
+          // Sending a status 404 message to the user if thought with the given id is not found
+          response.status(404).jason({
+            message: `Sorry, no though with id ${params.UserThoughtId} has been found! Please check your input and try again!`,
+          });
+          return;
+        }
+        res.json(databaseUserData);
+      })
+      .catch((err) => res.json(err));
+  },
 };
 
-module.exports = userController;
+module.exports = userthoughtController;
---
+++ routes/api/userThought-routes.js
@@ -0,0 +1,32 @@
+// Importing dependencies
+const router = require("express").Router();
+
+const {
+  getAllUserThought,
+  getUserThoughtById,
+  createUserThought,
+  updateUserThought,
+  deleteUseerThought,
+  createUserReaction,
+  deleteUserReaction,
+} = require("../../controllers/userThought-controller");
+
+// Configure GET all and POST to at /api/userThoughts
+router.route("/").get(getAllUserThought).post(createUserThought);
+
+// Configure GET, PUT and DELETE a userThought by id at /api/usersThoughts/:id
+router
+  .route("/:id")
+  .get(getUserThoughtById)
+  .put(updateUserThought)
+  .delete(deleteUseerThought);
+
+// Configure POST reaction by userThoughtId at /api/userThoughts/:userThoughtId/reactions
+router.route("/:userThoughtId/reactions").post(createUserReaction);
+
+// Configure DELETE reaction by reactionId at /api/userThoughts/:userThoughtId/reactions/:reactionId
+router
+  .route("/:userThoughtId/reactions/:reactionId")
+  .delete(deleteUserReaction);
+
+module.exports = router;
